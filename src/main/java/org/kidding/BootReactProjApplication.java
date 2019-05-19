de60342089becab26c745588961173d7247e73da
Kidding Just <rlarkdlr@gmail.com>
2019-05-19 19:52:08 +0900

    init

---
+++ src/main/java/org/kidding/BootReactProjApplication.java
@@ -0,0 +1,13 @@
+package org.kidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReactProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootReactProjApplication.class, args);
+	}
+
+}
